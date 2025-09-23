33cb12b6b595406a7379ca3c3b29c615ed641347
unknown <jinwook232@naver.com>
2025-09-23 13:24:47 +0900

    i got it

---
+++ Lecture04/Lecture04.c
@@ -0,0 +1,67 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+#define NUM 10
+
+int main()
+{
+	int i, j, data = 0;
+
+	int a[NUM];
+	int b[NUM];
+
+	for (i = 0;i < NUM;i++)
+	{
+		a[i] = (rand() % NUM);
+		for (j = 0;j < i;j++)
+		{
+			if (a[i] == a[j])
+			{
+				i--;
+			}
+		}
+	}
+
+	printf("a : ");
+	for (i = 0;i < NUM;i++)
+	{
+		printf("%d ", a[i]);
+	}
+
+	/*
+	for (i = 0; i < NUM - 1; i++)
+	{
+		for (j = i + 1; j < NUM; j++)
+		{
+			if (b[i] > b[j]) 
+			{
+				data = b[i];
+				b[i] = b[j];
+				b[j] = data;
+			}
+		}
+	}
+	*/
+
+	for (int i = 0; i < NUM; i++)
+	{
+		data = -1;
+		for (int j = 0; j < NUM; j++)
+		{
+			if (a[j] == i) 
+			{
+				data = j;
+				break;
+			}
+		}
+		b[i] = data;
+	}
+
+	printf("\nb : ");
+	for (i = 0;i < NUM;i++)
+	{
+		printf("%d ", b[i]);
+	}
+
+	return 0;
+}
