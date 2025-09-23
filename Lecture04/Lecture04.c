5fbc38b3124531ed0756bf40ec009f566858322b
4nor1ker <4nor1ker@gmai.com>
2025-09-23 17:07:15 +0900

    aa

--- Lecture04/Lecture04.c
+++ Lecture04/Lecture04.c
@@ -10,25 +10,19 @@ int main()
 	int a[NUM];
 	int b[NUM];
 
-	for (i = 0;i < NUM;i++)
-	{
+	for (i = 0;i < NUM;i++){
 		a[i] = (rand() % NUM);
-		for (j = 0;j < i;j++)
-		{
-			if (a[i] == a[j])
-			{
+		for (j = 0;j < i;j++){
+			if (a[i] == a[j]){
 				i--;
 			}
 		}
 	}
 
 	/*
-	for (i = 0; i < NUM - 1; i++)
-	{
-		for (j = i + 1; j < NUM; j++)
-		{
-			if (b[i] > b[j]) 
-			{
+	for (i = 0; i < NUM - 1; i++){
+		for (j = i + 1; j < NUM; j++){
+			if (b[i] > b[j]) {
 				data = b[i];
 				b[i] = b[j];
 				b[j] = data;
@@ -37,13 +31,10 @@ int main()
 	}
 	*/
 
-	for (int i = 0; i < NUM; i++)
-	{
+	for (int i = 0; i < NUM; i++){
 		data = -1;
-		for (int j = 0; j < NUM; j++)
-		{
-			if (a[j] == i) 
-			{
+		for (int j = 0; j < NUM; j++){
+			if (a[j] == i) {
 				data = j;
 				break;
 			}
@@ -52,8 +43,7 @@ int main()
 	}
 
 	printf("i   \ta\tb");
-	for (i = 0; i < NUM; i++)
-	{
+	for (i = 0; i < NUM; i++){
 		//Sleep(5);
 		printf("\n%d   \t%d\t%d", i, a[i], b[i]);
 	}
